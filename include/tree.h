c8cecb60c000b9cf3145bce18b0c7673205f74eb
AleksandrZzz <124875504+AleksandrZzz@users.noreply.github.com>
2023-06-09 21:22:31 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,53 +1,57 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
-#include <algorithm>
-#include  <iostream>
 #include <vector>
-
-
+#include <algorithm>
 class Tree {
- private:
     struct Node {
-        bool isRoot = false;
-        char value;
-        std::vector<Node*> Nvect;
+        char a;
+        std::vector<Node*> Nodes;
     };
-    Node* root;
-    std::vector<std::vector<char>> permutes;
-    void findPermutes(Node* root, std::vector<char> CHvect) {
-        if (!root->isRoot)
-            CHvect.push_back(root->value);
-        if (root->Nvect.empty()) {
-            permutes.push_back(CHvect);
-        } else {
-            for (Node* child : root->Nvect) {
-                findPermutes(child, CHvect);
+    std::vector<char> change;
+    Node* root = nullptr;
+    void make(std::vector<char> val, Node* root) {
+        for (auto i : val) {
+            if (i == root->a) {
+                val.erase(std::remove(val.begin(), val.end(), i),
+                    val.end());
+                for (auto ch : val) {
+                    Node* tmp = new Node;
+                    tmp->a = ch;
+                    root->Nodes.push_back(tmp);
+                }
             }
         }
-    }
-    void insert(Node* root, const std::vector<char>& CHvect) {
-        for (char ch : CHvect) {
-            Node* tmpl = new Node;
-            tmpl->value = ch;
-            root->Nvect.push_back(tmpl);
-            std::vector<char> Chars(CHvect);
-            Chars.erase(std::find(Chars.begin(), \
-                Chars.end(), ch));
-            insert(tmpl, Chars);
+        for (auto i : root->Nodes) {
+            make(val, i);
         }
     }
-
- public:
-    explicit Tree(const std::vector<char>& vect) {
-        root = new Node;
-        root->isRoot = true;
-        insert(root, vect);
-        std::vector<char> data;
-        findPermutes(root, data);
+    void Changes(Node* root) {
+        if (root->Nodes.empty()) {
+            change.push_back(root->a);
+            shifts.push_back(change);
+            change.clear();
+        }
+        for (auto i : root->Nodes) {
+            change.push_back(root->a);
+            Changes(i);
+        }
     }
-    std::vector<std::vector<char>> getAllPermutes() const {
-        return permutes;
+ public:
+    std::vector<std::vector<char>> shifts;
+    explicit Tree(std::vector<char> val) {
+        root = new Node;
+        root->a = ' ';
+        for (auto i : val) {
+            Node* tmp = new Node;
+            tmp->a = i;
+            root->Nodes.push_back(tmp);
+        }
+        for (auto i : root->Nodes) {
+            make(val, i);
+            Changes(i);
+            change.clear();
+        }
     }
 };
 #endif  // INCLUDE_TREE_H_
